4ced18d46acfa7248b8229058e0eba21abd95834
william2271 <william22711@gmail.com>
2024-06-04 11:56:29 -0400

    Initial commit

---
+++ src/Calculator.java
@@ -0,0 +1,24 @@
+public class Calculator {
+
+    public int add(int num1, int num2){
+
+        return num1 + num2;
+    }
+    public int subs(int num1, int num2){
+
+        return num1 / num2;
+    }
+    public int multi(int num1, int num2){
+
+        return num1 * num2;
+    }
+    public int divide(int num1, int num2){
+
+        return num1 / num2;
+    }
+    public int modulo(int num1, int num2){
+
+        return num1 % num2;
+    }
+
+}
---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+// Basic Calculator
+        Calculator Cal = new Calculator();
+
+System.out.println("The result is: "+Cal.add(10,20));
+        System.out.println("The result is: "+Cal.subs(10,20));
+        System.out.println("The result is: "+Cal.divide(10,20));
+        System.out.println("The result is: "+Cal.modulo(10,20));
+        System.out.println("The result is: "+Cal.multi(10,20));
+
+        System.out.println("Hello world!");
+    }
+}
